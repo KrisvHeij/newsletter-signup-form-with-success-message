2b3e40fb85747515c0901a31dcfe6cf241c44b49
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2025-06-24 22:31:16 +0200

    Added error state classes

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const containerEl = document.querySelector(".container");
 const validationErrorText = document.getElementById("validation-error");
-const email = document.getElementById("email");
+const emailInput = document.getElementById("email");
 const submitBtn = document.getElementById("submit-btn");
 const successMsg = document.getElementById("success-msg");
 
 const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function validateEmail() {
+  const email = emailInput.value;
+  const validEmail = regex.test(email);
+
+  if(email === "" || !validEmail) {
+    console.log("please provide a valid email");
+  }
+  
+}
+
+
+submitBtn.addEventListener("click", checkEmail);
