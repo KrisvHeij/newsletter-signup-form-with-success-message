e8dc98fa39f2a56f9722e5c4b79cdc09a9110c19
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2025-06-25 21:35:01 +0200

    Added open & close funtionality for message element

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const containerEl = document.querySelector(".container");
 const validationErrorText = document.getElementById("validation-error");
 const emailInput = document.getElementById("email");
 const submitBtn = document.getElementById("submit-btn");
+const emailConfirm = document.getElementById("email-confirm");
+const dismissBtn = document.getElementById("dismiss-btn");
 const successMsg = document.getElementById("success-msg");
 
 const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -14,12 +16,32 @@ function validateEmail() {
     validationErrorText.classList.remove("hidden");
     validationErrorText.classList.add("show");
     emailInput.classList.add("validation-error-email");
-  } 
+  } else {
+    showCloseMessage();
+    emailInput.value = "";
+    emailConfirm.textContent = email;
+  }
+}
+
+function showCloseMessage() {
+  if (!containerEl.classList.contains("hidden")) {
+    containerEl.classList.add("hidden");
+    successMsg.classList.remove("hidden");
+    successMsg.classList.add("show");
+  } else {
+    containerEl.classList.remove("hidden");
+    successMsg.classList.remove("show");
+    successMsg.classList.add("hidden");
+  }
 }
 
 
-emailInput.addEventListener("change", () => {
-  console.log("change")
-});
+
+
+// emailInput.addEventListener("blur", () => {
+//   console.log("input changed")
+// });
 
 submitBtn.addEventListener("click", validateEmail);
+
+dismissBtn.addEventListener("click", showCloseMessage);
